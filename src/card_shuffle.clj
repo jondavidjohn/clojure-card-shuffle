373cb356d3878ce90a904d63f3915abba6f7e6a8
Jonathan Johnson <jon@crowdfavorite.com>
2012-12-01 10:59:57 -0700

    cleaning up, refactoring out partition-by-rand helper

--- src/card_shuffle.clj
+++ src/card_shuffle.clj
@@ -1,47 +1,51 @@
 (ns card-shuffle)
 
+;; Helper to Chunk coll into random groups
+(defn- partition-by-rand [coll]
+  (partition-by (fn [i] (= 0 (rand-int 2))) coll))
+
 (defn riffle
   ([cards] (riffle cards 1))
   ([cards times]
-    (let [card_count (count cards)
-          marg (if (> card_count 20) (int (* 0.1 card_count)) 2)
+    (let [card-count (count cards)
+          marg (if (> card-count 20) (int (* 0.1 card-count)) 2)
           middleish (+ (int (/ card_count 2)) (rand-nth (range (- marg) marg)))
-          decks (split-at middleish cards)
-          deck1 (->> (nth decks 0) (partition-by (fn [i] (= 0 (rand-int 2)))))
-          deck2 (->> (nth decks 1) (partition-by (fn [i] (= 0 (rand-int 2)))))]
+          [deck1 deck2] (split-at middleish cards)
+          deck1 (partition-by-rand deck1)
+          deck2 (partition-by-rand deck2)]
       (->> (interleave deck1 deck2) flatten))))
+
 
 (defn overhand
   ([cards] (overhand cards 1))
   ([cards times]
-    (let [card_count (count cards)
-          shuffled_deck (->> cards
-            (partition-by (fn [c] (= 0 (rand-int 2) )))
+    (let [shuffled-deck (->> cards
+            partition-by-rand
             reverse
             flatten)]
-    (if (> times 1)
-        (overhand shuffled_deck (dec times))
-        shuffled_deck))))
+      (if (> times 1)
+        (recur shuffled-deck (dec times))
+        shuffled-deck))))
 
 (defn mongean
   ([cards] (mongean cards 1))
   ([cards times]
-    (let [vec_cards (vec cards)
-          num-cards (count vec_cards)
+    (let [vec-cards (vec cards)
+          num-cards (count vec-cards)
           final-order (concat (reverse (range 1 num-cards 2))
                               (range 0 num-cards 2))]
       (if (> times 1)
-        (mongean (replace vec_cards final-order) (dec times))
-        (seq (replace vec_cards final-order))))))
+        (recur (replace vec-cards final-order) (dec times))
+        (seq (replace vec-cards final-order))))))
 
 (defn pile
   ([cards] (pile cards 3 1))
-  ([cards num_piles] (pile cards num_piles 1))
-  ([cards num_piles times]
-    (let [shuffled_cards (->>
-            (range (dec num_piles) -1 -1)
-            (mapcat #(take-nth num_piles (drop % cards)))
+  ([cards num-piles] (pile cards num-piles 1))
+  ([cards num-piles times]
+    (let [shuffled-cards (->>
+            (range (dec num-piles) -1 -1)
+            (mapcat #(take-nth num-piles (drop % cards)))
             reverse)]
       (if (> times 1)
-        (pile shuffled_cards num_piles (dec times))
-        shuffled_cards))))
+        (recur shuffled-cards num-piles (dec times))
+        shuffled-cards))))
