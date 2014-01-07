5facb15215ccc19dc72ce4f298924ce4ab7880e1
Jonathan Johnson <jon@crowdfavorite.com>
2014-01-07 08:59:14 -0700

    add tests for result length

--- test/card_shuffle_test.clj
+++ test/card_shuffle_test.clj
@@ -2,6 +2,20 @@
   (:require [card-shuffle :refer :all])
   (:require [clojure.test :refer :all]))
 
+(deftest riffle-test
+  (is (= (count (riffle [1 2 3 4 5 6])) 6)) ;; even
+  (is (= (count (riffle (seq [1 2 3 4 5]))) 5)) ;; odd
+  (is (= (count (riffle '(1 2 3 4 5 6) 3)) 6)) ;; recur even
+  (is (= (count (riffle [1 2 3 4 5] 3)) 5)) ;; recur odd
+  (is (= (count (riffle '(1 2 3 4 5) 2)) 5))) ;; recur odd
+
+(deftest overhand-test
+  (is (= (count (overhand [1 2 3 4 5 6])) 6)) ;; even
+  (is (= (count (overhand (seq [1 2 3 4 5]))) 5)) ;; odd
+  (is (= (count (overhand '(1 2 3 4 5 6) 3)) 6)) ;; recur even
+  (is (= (count (overhand [1 2 3 4 5] 3)) 5)) ;; recur odd
+  (is (= (count (overhand '(1 2 3 4 5) 2)) 5))) ;; recur odd
+
 (deftest mongean-test
   (is (= (mongean [1 2 3 4 5 6]) (seq [6 4 2 1 3 5]))) ;; even
   (is (= (mongean (seq [1 2 3 4 5])) (seq [4 2 1 3 5]))) ;; odd
